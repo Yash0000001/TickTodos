ad4bcac89a46baedbb7d2f007cd95693b2b12a85
Yash0000001 <yashqwe58@gmail.com>
2024-12-17 17:24:15 +0530

    build error from config removed

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-    appDir: true,
+    // appDir: true,
   },
   images: {
     domains: ["example.com"], // Add your image domains
