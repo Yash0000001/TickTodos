192e69bda9c61231467c54e4e6f502926d171ccd
Yash0000001 <yashqwe58@gmail.com>
2024-12-19 13:08:17 +0530

    layout change for pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
-"use client"
+// "use client"
 import { dark } from "@clerk/themes";
 import { ClerkLoaded, ClerkLoading, ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
 import "./globals.css";
-import dynamic from "next/dynamic";
-const PwaUpdater = dynamic(() => import(`./PwaUpdater`), { ssr: false });
+// import dynamic from "next/dynamic";
+import Head from "next/head";
+// const PwaUpdater = dynamic(() => import(`./PwaUpdater`), { ssr: false });
 
 
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         
         <title>TickTodos</title>
         <meta name="description" content="Manage your todos" />
@@ -27,8 +28,8 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="TickTodos" />
-      </head>
-      <PwaUpdater/>
+      </Head>
+      {/* <PwaUpdater/> */}
       <ClerkProvider appearance={{ baseTheme: dark }}>
         <body>
           
