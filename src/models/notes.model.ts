150035cb713ea7ea299ca95479be50a42e96bfd9
Yash0000001 <yashqwe58@gmail.com>
2025-08-03 16:56:25 +0530

    update: notes model updated and script already runned

--- src/models/notes.model.ts
+++ src/models/notes.model.ts
@@ -3,6 +3,8 @@ import {Schema,model,models,Document,Model} from "mongoose"
 export interface INotes extends Document {
     title: string;
     message: string;
+    favourite: boolean;
+    pinned: boolean;
     createdAt:Date;
     updatedAt:Date;
   }  
@@ -14,8 +16,15 @@ const notesSchema:Schema<INotes> = new Schema<INotes>({
     },
     message:{
         type:String,
+    },
+    favourite:{
+        type:Boolean,
+        default: false
+    },
+        pinned:{
+        type:Boolean,
+        default: false
     }
-
 },{timestamps:true})
 
 export const notes:Model<INotes> = models.notes || model<INotes>('notes',notesSchema);
